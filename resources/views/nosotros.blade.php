51a48e1c8a2924c04ad489803739b6b11bcc3dbf
Norman Arredondo Martinez <arredondo_norman25@outlook.es>
2020-10-02 21:23:13 -0500

    Trabajando con ROUTING Y CONTROLES además de Invokable Controler pero sólo creando con

---
+++ resources/views/nosotros.blade.php
@@ -0,0 +1,14 @@
+<h1>Desde nosotros.blade.php</h1>
+
+@php
+$variable =20;
+
+
+@endphp
+
+@if ($variable ===20)
+    {{"sí, es 20"}}
+
+@endif
+
+{{$variable}}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//--  /nosotros es de blade, Nombre del controlller, se tiene que agregar un método @método
+// Va a ir al controlador y después al método de ese controlador
+
+Route::get('/nosotros', 'RecetaController');
+
+//Lo que hicimos antes de agregar el controlador
+/*
+Route::get('/nosotros', function () {
+    return view('nosotros');
+});
+*/
+
+//Otra forma
+/*Route::get('/nosotros', function () {
+    return "Desde Nosotros";
+});
+
+Pero la forma adecuada es hacer un controller
+*Para crear algo lo mejor es hacerlo desde artisan
+ * en consola: php artisan serve make:controller RecetaController
+ *
+ * Reglas: El nombre del controlador es una clase, por lo tanto siempre va en mayusculas la primer letra
+ * los controladores de artisan se encuentran en la carpeta app/http/controller
+
+ * */
+
+
+/*Accedemos al controlador y le decímos qué método va a cargar*/
